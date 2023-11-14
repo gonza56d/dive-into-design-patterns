4591929370aab6da20e47dd8756dc878af9f5352
Gonza <gonza56d@gmail.com>
2023-11-14 17:24:51 -0300

    test builder working from api

--- tests/test_builder.py
+++ tests/test_builder.py
@@ -4,7 +4,7 @@ from fastapi.testclient import TestClient
 import pytest
 
 from dive.api.main import app
-from dive.core.builder.cars import Car
+from dive.core.builder.cars import Car, Color
 from dive.core.exceptions import NotFound
 from dive.core.mongo_repo import MongoRepo
 
@@ -34,3 +34,34 @@ class TestBuilder:
     def test_get_car_not_found(self):
         with pytest.raises(NotFound):
             self.client.get(f'{self.url}/{self.owner_name}')
+
+    def test_update_car_parts_success(self):
+        expected_car = Car(owner_name=self.owner_name)
+        self.client.post(f'{self.url}/{self.owner_name}')
+
+        response = self.client.get(f'{self.url}/{self.owner_name}')
+        assert response.json() == asdict(expected_car)
+
+        self.client.patch(
+            f'{self.url}/{self.owner_name}',
+            json={'command': 'set_color', 'value': 'RED'},
+        )
+        expected_car.color = Color.RED
+        response = self.client.get(f'{self.url}/{self.owner_name}')
+        assert response.json() == asdict(expected_car)
+
+        self.client.patch(
+            f'{self.url}/{self.owner_name}',
+            json={'command': 'set_air_conditioner'},
+        )
+        expected_car.air_conditioner = True
+        response = self.client.get(f'{self.url}/{self.owner_name}')
+        assert response.json() == asdict(expected_car)
+
+        self.client.patch(
+            f'{self.url}/{self.owner_name}',
+            json={'command': 'set_vin', 'value': 'MY_TESTING_VIN'},
+        )
+        expected_car.vin = 'MY_TESTING_VIN'
+        response = self.client.get(f'{self.url}/{self.owner_name}')
+        assert response.json() == asdict(expected_car)
