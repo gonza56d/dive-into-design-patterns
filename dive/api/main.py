b3997cf506c686400bc9bf0ca93f61644f37b67a
Gonza <gonza56d@gmail.com>
2023-11-14 10:25:22 -0300

    handle errors

--- dive/api/main.py
+++ dive/api/main.py
@@ -1,11 +1,25 @@
 """Main module to run the FastAPI application."""
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import JSONResponse
 
 from dive.api.routers import abstract_factory, builder, hello_world
+from dive.core.exceptions import ClientError
 
 
 app = FastAPI()
 app.include_router(abstract_factory)
 app.include_router(builder)
 app.include_router(hello_world)
+
+@app.exception_handler(Exception)
+async def exception_handler(request: Request, err: Exception) -> JSONResponse:
+    if isinstance(err, ClientError):
+        return JSONResponse(
+            status_code=err.status_code,
+            content={'error_message': err.message}
+        )
+    return JSONResponse(
+        status_code=err.status_code if hasattr(err, 'status_code') else 500,
+        content={'error_message': err.message if hasattr(err, 'message') else 'Internal server error.'}
+    )
--- dive/core/exceptions.py
+++ dive/core/exceptions.py
@@ -1,5 +1,27 @@
+from abc import ABC, abstractmethod
 
-class NotFound(Exception):
+
+class ClientError(ABC, Exception):
     
+    @property
+    def status_code(self) -> int:
+        return 400
+
+    @property
+    @abstractmethod
+    def message(self) -> str:
+        pass
+
+
+class NotFound(ClientError):
+
     def __init__(self) -> None:
-        super().__init__('Not found.')
+        super().__init__(self.message)
+
+    @property
+    def status_code(self) -> int:
+        return 404
+
+    @property
+    def message(self) -> str:
+        return 'Resource not found.'
